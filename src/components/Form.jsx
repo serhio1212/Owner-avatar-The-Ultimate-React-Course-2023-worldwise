6d189acfb3c051d4bc82f192138102d963a7ef6f
serhio1212 <zwezda-11@mail.ru>
2024-10-15 01:09:21 +0400

    feat(map): setting map position with geolocation
    Finish job, all works fine
    
    18/14

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,25 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from './Button.jsx';
-import BackButton from './BackButton.jsx';
-import styles from './Form.module.css';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button.jsx";
+import BackButton from "./BackButton.jsx";
+import styles from "./Form.module.css";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
-    .split('')
+    .split("")
     .map((char) => 127397 + char.charCodeAt());
   return String.fromCodePoint(...codePoints);
 }
 
 function Form() {
   const navigate = useNavigate();
-  const [cityName, setCityName] = useState('');
-  const [country, setCountry] = useState('');
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
-  const [notes, setNotes] = useState('');
+  const [notes, setNotes] = useState("");
+
 
   return (
     <form className={styles.form}>
